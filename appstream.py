5485ee9862edb6b39fd8a93ecdf1916118e4cb5d
Precious Okosiako <precious.okosiako@sage.com>
2021-10-20 16:04:18 +0100

    update for appstream.py

--- appstream.py
+++ appstream.py
@@ -2,14 +2,15 @@
 
 import boto3
 import time
-#telling boto3 which aws client api to connect to
+
+#telling boto3 which reasources api to connect to
 
 client = boto3.client('appstream')
 
 #storing user information in a dictionary
 
 user_details = {
-    "UserName": "tegaokosiako@gmail2.com",
+    "UserName": "prechieb@gmail",
     "MessageAction":  "SUPPRESS",
     "FirstName": "Testing",
     "LastName": "LastnameTesting",
@@ -17,8 +18,7 @@ user_details = {
 
 }
 
-# creating user with the client(appstream)
-
+#creating a new user in the userpool
 
 create_user = client.create_user(
      UserName = user_details["UserName"],
@@ -27,18 +27,8 @@ create_user = client.create_user(
      LastName = user_details["LastName"],
      AuthenticationType = user_details["AuthenticationType"]
  )
-
+#Giving some time to let client create user before assign stack.
 time.sleep(100)
-assign_user_stack = client.batch_associate_user_stack(
-    UserStackAssociations=[
-        {
-            "StackName": "precious-appstream-stack",
-            "UserName": user_details["UserName"],
-            "AuthenticationType": "USERPOOL",
-            "SendEmailNotification": True
-        },
-    ]
-)
 
 
 # assigning a stack to the user created
